354780429e5b595c719c56cda9226910d59228e1
derriba <jp.sepulveda@yahoo.com>
2015-07-17 00:26:41 -0300

    Diseño nuevo a archivo crusos/create

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -1,3 +1,200 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>Sistema</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
+    <!--external css-->
+    <link href="{{ asset('/css/font-awesome.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/zabuto_calendar.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/js/gritter/css/jquery.gritter.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('/lineicons/style.css') }}">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/style-responsive.css') }}" rel="stylesheet">
+
+    <script src="{{ asset('/js/chart-master/Chart.js') }}"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+      body {overflow-x:hidden;}
+    </style>
+  </head>
+
+  <body>
+
+  <section id="container" >
+      <!-- **********************************************************************************************************************************************************
+      TOP BAR CONTENT & NOTIFICATIONS
+      *********************************************************************************************************************************************************** -->
+      <!--header start-->
+      <header class="header black-bg">
+
+            <!--logo start-->
+            <a href="index.html" class="logo">{!!HTML::image('utem.png')!!}</a>
+            <!--logo end-->
+            <div class="nav notify-row" id="top_menu">
+                <!--  notification start -->
+                <ul class="nav top-menu">
+                    <!-- settings start -->
+
+                    <!-- settings end -->
+                    <!-- inbox dropdown start-->
+
+                    <!-- inbox dropdown end -->
+                </ul>
+                <!--  notification end -->
+            </div>
+            <div class="top-menu">
+              <ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="login.html">Logout</a></li>
+              </ul>
+            </div>
+        </header>
+      <!--header end-->
+
+      <!-- **********************************************************************************************************************************************************
+      MAIN SIDEBAR MENU
+      *********************************************************************************************************************************************************** -->
+      <!--sidebar start-->
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+              <br><br>
+
+                  <h5 class="centered">Menú Encargado</h5>
+
+                  <li class="mt">
+                      <a href="encargado/menu">
+                          <span>INICIO</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="/asignaturas" >
+                          <span>ASIGNATURAS</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a class="active" href="/cursos" >
+                          <span>CURSOS</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="/horarios" >
+                          <span>HORARIO</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="/salas" >
+                          <span>SALAS</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="/tiposdesalas" >
+                          <span>TIPOS DE SALAS</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="/periodos" >
+                          <span>PERIODOS</span>
+                      </a>
+                  </li>
+
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
+      <!--sidebar end-->
+      <section id="main-content">
+          <section class="wrapper">
+          <br>
+            <h3> Registrar Curso</h3>
+
+            <!-- BASIC FORM ELELEMNTS -->
+            <div class="row mt">
+              <div class="col-lg-12">
+                  <div class="form-panel">
+                    @if (count($errors) > 0)
+	                    <div class="alert alert-danger">
+	                      <ul>
+	                        @foreach ($errors->all() as $error)
+	                           <li>{{ $error }}</li>
+                          @endforeach
+	                      </ul>
+                      </div>
+                    @endif
+                      <h4 class="mb">Ingrese la información del Curso</h4>
+                      {!! Form::open(['route' => 'cursos.store']) !!}
+                      <form class="form-horizontal style-form" method="get">
+                      		<label class="col-sm-2 col-sm-2 control-label">Semestre: </label>
+                              <div class="col-sm-10">
+                                  {!! Form::text('semestre', null, ['class' => 'form-control', 'placeholder'=>'Semestre']) !!}
+                              </div>
+                              <br><br><br>
+                              <label class="col-sm-2 col-sm-2 control-label">Año: </label>
+                              <div class="col-sm-10">
+                                  {!! Form::text('anio', null, ['class' => 'form-control', 'placeholder'=>'Año']) !!}
+                              </div>
+                              <br><br><br>
+                              <label class="col-sm-2 col-sm-2 control-label">Sección: </label>
+                              <div class="col-sm-10">
+                                  {!! Form::text('seccion', null, ['class' => 'form-control', 'placeholder'=>'Sección']) !!}
+                              </div>
+                              <br><br><br>
+                              <label class="col-sm-2 col-sm-2 control-label">Asignatura: </label>
+                              <div class="col-sm-10">
+                                  {!! Form::select('asignatura_id', $asignaturas) !!}
+                              </div>
+                              <br><br><br>
+                              <label class="col-sm-2 col-sm-2 control-label">Docente: </label>
+                              <div class="col-sm-10">
+                                  {!! Form::select('docente_id', $docente) !!}
+                              </div>
+							<div class="form-group">
+								{!! Form::submit('Registrar', ["class" => "btn btn-success btn-block"]) !!}
+							</div>
+					</form>
+							{!! Form::close() !!}
+      						<p>
+	    						@if(Session::has('message'))
+          						<div class="btn btn-success disabled{{ Session::get('class') }}">{{ Session::get('message')}}</div>
+        						@endif
+      						</p>
+                  </div>
+              </div><!-- col-lg-12-->
+            </div><!-- /row -->
+
+<center>
+<br>
+</center>
+    </section>
+      </section>
+
+	</body>
+
+</html>
+
+
+
 @extends('encargado.plantillaEncargado')
 @section('contenido')
 <ul class="nav nav-tabs">
